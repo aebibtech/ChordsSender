4354c4079b18e3bcfe1520052234549763528c52
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-11-12 06:47:38 +0800

    Refactor: getting latest chord folder

--- ChordsSender/PdfMerger.cs
+++ ChordsSender/PdfMerger.cs
@@ -10,15 +10,16 @@ namespace ChordsSender
     public class PdfMerger
     {
         private AppSettings _settings;
+        
         public PdfMerger(AppSettings settings)
         {
             _settings = settings;
         }
+
         public string MergePdf()
         {
-            var latestChordsPath = Directory.EnumerateDirectories(_settings.PDfPath).Last();
-            var dateFileName = latestChordsPath.Split('\\').Last();
-            var outputFileName = dateFileName + "_chords.pdf";
+            var latestChordsPath = new DirectoryInfo(_settings.PDfPath).GetDirectories().OrderByDescending(d => d.LastWriteTime).Select(d => d.Name).First();
+            var outputFileName = latestChordsPath + "_chords.pdf";
             var files = Directory.EnumerateFiles(Path.Combine(_settings.PDfPath, latestChordsPath)).ToArray();
             var outputPath = Path.Combine(_settings.PDfPath, outputFileName);
 
